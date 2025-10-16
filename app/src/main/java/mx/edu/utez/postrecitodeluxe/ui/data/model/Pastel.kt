2181faecd8a804e2a1234f142da23c8fc36c8a8e
Evelinmq <20243ds170@utez.edu.mx>
2025-10-16 11:54:28 -0600

    PantallaPrincipal

---
+++ app/src/main/java/mx/edu/utez/postrecitodeluxe/ui/data/model/Pastel.kt
@@ -0,0 +1,12 @@
+package mx.edu.utez.postrecitodeluxe.ui.data.model
+
+class Pastel (
+    val sabor: String,
+    val glaseado: String,
+    val topping: String,
+    val relleno: String,
+    val tamanio: String,
+    val forma: String
+){
+
+}
